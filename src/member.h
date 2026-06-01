e916a26512f4dbdac9c4bad90ee8d0b77b12d227
mustafabaihaqi <mustafabaihaqi@gmail.com>
2026-06-01 20:23:31 +0700

    feat: add working admin manage members operation

--- src/member.h
+++ src/member.h
@@ -1,24 +1,18 @@
-#pragma once
-#include "User.h"
+#ifndef MEMBER_H
+#define MEMBER_H
 #include <string>
 
-class Member : public User {
+class Member {
 private:
-    std::string memberId_;
+    std::string id_;
     std::string name_;
     std::string email_;
 
 public:
-    Member(std::string username, std::string passwordHash, std::string memberId, std::string name, std::string email);
-    std::string getMemberId() const;
+    Member(std::string id, std::string name, std::string email);
+    std::string getId() const;
     std::string getName() const;
     std::string getEmail() const;
-
-    // Override metode virtual
-    std::string role() const override;
-    const std::string& getMemberId() const;
-
-    // Prototipe logika peminjaman
-    void borrowItem() const;
-    void returnItem() const;
 };
+
+#endif
